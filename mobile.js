616c1dddd45354aa3d275ad3e30ffc7f9a20bda0
bielkawan <bielkawan400@gmail.com>
2024-07-30 00:13:11 -0300

    primeiro commit

---
+++ mobile.js
@@ -0,0 +1,26 @@
+const hamburguer = document.querySelector("#hamburguer");
+const navMenu = document.querySelector("#menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-list").forEach((n) =>
+    n.addEventListener("click", () => {
+        hamburguer.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
+
+    
+    const intersectObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            console.log(entry)
+            entry.target.classList.toggle("show", entry.isIntersecting)
+        })
+    })
+    
+    const elements = document.querySelectorAll(".box")
+    
+    elements.forEach(el => intersectObserver.observe(el))
